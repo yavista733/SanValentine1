91d9c68ecd9a6ef28135aa1f9b399a93ebcf86bf
jsramirezd <kairo_seven7@hotmail.com>
2025-02-13 02:25:47 -0500

    Actualización del proyecto

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,11 +3,11 @@ const btnNo = document.querySelector('.botones__btn-no');
 const randomPic = document.querySelector('.random-pics__cats');
 const question = document.querySelector('.question');
 
-const musica = new Audio('/assets/musica/dragonBallGT.mp3');
+const musica = new Audio('./assets/musica/dragonBallGT.mp3');
 
 // Si acepta: 
 btnSi.addEventListener('click', ()=>{
-    randomPic.src = '/assets/fotos/8.jpg';
+    randomPic.src = './assets/fotos/8.jpg';
     question.innerText = ' Siempre Juntos Amorcito    ---------------------- Shairf & Maricielo !!!!! <3 ';
     question.classList.add('question-style');
 
@@ -25,48 +25,40 @@ btnSi.addEventListener('click', ()=>{
 
 let contador = 0;
 
-btnNo.addEventListener('click', ()=>{
-
-        switch (contador) {
-            
-            case 0:
-                btnNo.innerText = 'Segura?';
-                btnSi.classList.add('estilo-1');
-                randomPic.src = '/assets/fotosgit branch -M main/2.jpg'
-                break;
-            
-            case 1:
-                btnNo.innerText = 'Segurísima?';
-                btnSi.classList.add('estilo-2');
-                randomPic.src = '/assets/fotos/3.jpg'
-                break;
-        
-            case 2:
-                btnNo.innerText = 'Piensalo :( ?';
-                btnSi.classList.add('estilo-3');
-                randomPic.src = '/assets/fotos/4.jpg'
-
-                break;
-
-            case 3:
-                btnNo.innerText = 'No te voy a rogar...';
-                btnSi.classList.add('estilo-4');
-                randomPic.src = '/assets/fotos/5.jpg'
-
-                break;
-
-            case 4:
-                btnNo.innerText = 'Ojala te muerda la Emma... ';
-                btnSi.classList.add('estilo-5');
-                randomPic.src = '/assets/fotos/7.jpg'
-
-                break;
-
-        }
-
-        contador++
-        
-})
+btnNo.addEventListener('click', () => {
+    if (contador > 4) {
+        btnNo.innerText = 'Ok... ya entendí 😔';
+        return;
+    }
+    switch (contador) {
+        case 0:
+            btnNo.innerText = 'Segura?';
+            btnSi.classList.add('estilo-1');
+            randomPic.src = './assets/fotos/2.jpg';
+            break;
+        case 1:
+            btnNo.innerText = 'Segurísima?';
+            btnSi.classList.add('estilo-2');
+            randomPic.src = './assets/fotos/3.jpg';
+            break;
+        case 2:
+            btnNo.innerText = 'Piensalo :( ?';
+            btnSi.classList.add('estilo-3');
+            randomPic.src = './assets/fotos/4.jpg';
+            break;
+        case 3:
+            btnNo.innerText = 'No te voy a rogar...';
+            btnSi.classList.add('estilo-4');
+            randomPic.src = './assets/fotos/5.jpg';
+            break;
+        case 4:
+            btnNo.innerText = 'Ojala te muerda la Emma... ';
+            btnSi.classList.add('estilo-5');
+            randomPic.src = './assets/fotos/7.jpg';
+            break;
+    }
+    contador++;
+});
 
 
 
